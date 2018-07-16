b1afd91b994339228b58026ee591b030087beb6a
diorelle <diorelle@screenfeed.com>
2018-07-16 18:54:41 -0500

    functioning daily page and beginings of controllers

--- pic-me-react/client/src/pages/QuestionPage/Question.js
+++ pic-me-react/client/src/pages/QuestionPage/Question.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import Nav from "../../components/Nav";
 import questions from "../../utils/Questions.json";
 import moment from "moment";
+import API from "../../utils/API";
 
 class Question extends Component {
     state = {
         questions,
         currentQ: questions[0].question,
-        time: new Date().toLocaleString()  
+        time: new Date().toLocaleString(),
+        search:"",
+        photo: "https://vignette.wikia.nocookie.net/uncyclopedia/images/0/01/DramaticQuestionMark.png/revision/latest?cb=20060419021703",
     };
     
     componentDidMount = () => {
@@ -30,7 +33,28 @@ class Question extends Component {
             this.randomQuestion()
         }
     
-    }
+    };
+
+    handleFormSubmit = event => {
+
+        event.preventDefault();
+
+        console.log("Searching...");
+        
+        API.getManyGif(this.state.search)
+            .then(res => {
+                console.log(res.data.data)
+                const randomGiphy = (arr) => Math.floor(Math.random() * arr.length)
+                this.setState( { photo: res.data.data[randomGiphy(res.data.data)].images.original.url }, () => console.log(this.state.photo))
+            })
+    };
+
+    handleInputChange = event => {
+
+        event.preventDefault();
+        console.log(event);
+        this.setState({search: event.target.value})
+    };
     
     randomQuestion() {
         let j = Math.floor(Math.random()*questions.length);
@@ -39,9 +63,9 @@ class Question extends Component {
         console.log(question);
         // this.setState({currentQ: question});
         if (this.state.currentQ === question) {
-          this.randomQuestion() 
+            this.randomQuestion() 
         } else {
-          this.setState({currentQ:question})
+            this.setState({currentQ:question})
         } 
     }
     
@@ -53,15 +77,53 @@ class Question extends Component {
                     <p>
                     {this.state.currentQ}
                     </p>
+                <div>
                     <form>
+                        <label> Answer:
+                            <input 
+                            type="text" 
+                            name="search"
+                            value={this.state.search}
+                            onChange={this.handleInputChange}
+                            />
+                        </label>
+                        <button 
+                            id="getGif"
+                            disabled={!(this.state.search)}
+                            onClick={this.handleFormSubmit}>
+                            Submit
+                        </button>  
+                    </form>
+                </div>
+                
+                <div>
+                    <img
+                        alt="404 Please Search Again"
+                        src={this.state.photo}
+                    />
+                    {/* <ImageCard
+                        src = {this.state.photo}
+                    /> */}
+                </div>
+                    {/* <form>
                         <label> Answer:
                         <input type="text" name="name" />
                         </label>
                     </form>
-                <button id="getDaily">Get Daily</button>
+                <button id="getDaily">Get Daily</button> */}
+                        <div className="Randomize">
+                <button class="btn btn-danger btn-lg" id="randomize-btn">Randomize</button>
             </div>
-        );
-    }
-} 
+            <div className="Share">
+                <button class="btn btn-danger btn-lg" id="share-btn">Share</button>
+            </div>
+            <div className="Noshare">
+            <button class="btn btn-danger btn-lg" id="noshare-btn">Don't Share</button>
+            </div>
+            </div>
+
+        
+        )};
+}
 
 export default Question;
