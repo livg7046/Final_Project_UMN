097889fa5117caac19d5d897e3902ba72841c40a
diorelle <diorelle@screenfeed.com>
2018-07-17 13:59:26 -0500

    randomize button and timer styling

--- pic-me-react/client/src/pages/QuestionPage/Question.js
+++ pic-me-react/client/src/pages/QuestionPage/Question.js
@@ -9,30 +9,71 @@ class Question extends Component {
         questions,
         currentQ: questions[0].question,
         time: new Date().toLocaleString(),
+        currentDate: moment(new Date()).format("M/DD/YYYY"),
         search:"",
         photo: "https://vignette.wikia.nocookie.net/uncyclopedia/images/0/01/DramaticQuestionMark.png/revision/latest?cb=20060419021703",
+        days: 0,
+        hours: 0,
+        min: 0,
+        sec: 0
     };
     
     componentDidMount = () => {
-        this.intervalID = setInterval(
-            () => this.tick(),
-            1000
-          );
-        };
-        componentWillUnmount() {
-        clearInterval(this.intervalID);
+        
+        this.intervalID = setInterval(() => this.tick(), 1000);
+        this.dateID = setInterval(() => this.dateTicker(moment().add(1,'days').startOf('day')), 1000)
     };
+    componentWillUnmount() {
+        clearInterval(this.intervalID);
+        clearInterval(this.dateID)
+    };
+
+    stop() {
+        clearInterval(this.dateID)
+        // make pretty things appear
+    }
+
+    dateTicker(midnight) {
+        let diff = (Date.parse(midnight) - Date.parse(new Date())) / 1000;
+        
+        if (diff <= 0) this.stop();
+
+        const timeLeft = {
+            days: 0,
+            hours: 0,
+            min: 0,
+            sec: 0
+          };
+    
+        if (diff >= 86400) { // 24 * 60 * 60
+        timeLeft.days = Math.floor(diff / 86400);
+        diff -= timeLeft.days * 86400;
+        }
+        if (diff >= 3600) { // 60 * 60
+        timeLeft.hours = Math.floor(diff / 3600);
+        diff -= timeLeft.hours * 3600;
+        }
+        if (diff >= 60) {
+        timeLeft.min = Math.floor(diff / 60);
+        diff -= timeLeft.min * 60;
+        }
+        timeLeft.sec = diff;
+    
+        this.setState({...this.state, ...timeLeft})
+        // console.log(this.state)
+    
+    }
   
     tick() {
         /* condition to check if current time is midnight.  iF so, run this.randomQuestion*/
         this.setState({
             time: new Date().toLocaleString()
         });
-        const currentDate = moment(new Date()).format("M/DD/YYYY");
-        if (this.state.time ==`${currentDate}, 12:54:30 PM`) {
+        // const currentDate = moment(new Date()).format("M/DD/YYYY");
+        if (this.state.time ===`${this.state.currentDate}, 12:54:30 PM`) {
             this.randomQuestion()
         }
-    
+        
     };
 
     handleFormSubmit = event => {
@@ -70,10 +111,14 @@ class Question extends Component {
     }
     
     render() {
+  
         return (
             <div className="container">
                 <Nav />
-                <h1 className="clock">{this.state.time}</h1>
+                {/* <h1 className="clock">{this.state.time}</h1> */}
+                <h2 className="clock">{this.state.currentDate}</h2>
+                <h3 className="timer">{this.state.hours} Hours {this.state.min} Minutes {this.state.sec} Seconds Remaining!
+                </h3>
                     <p>
                     {this.state.currentQ}
                     </p>
@@ -112,7 +157,7 @@ class Question extends Component {
                     </form>
                 <button id="getDaily">Get Daily</button> */}
                         <div className="Randomize">
-                <button class="btn btn-danger btn-lg" id="randomize-btn">Randomize</button>
+                <button class="btn btn-danger btn-lg" id="randomize-btn" onClick={this.handleFormSubmit}>Randomize</button>
             </div>
             <div className="Share">
                 <button class="btn btn-danger btn-lg" id="share-btn">Share</button>
--- pic-me-react/models/User.js
+++ pic-me-react/models/User.js
@@ -1,31 +1,60 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
 
 const Schema = mongoose.Schema; 
 
-const UsersSchema = new Schema({
+const UserSchema = new Schema({
     name: {
         type: String,
         required: true
     },
     userName: {
         type: String, 
-        require: true
+        required: true,
+        unique: true
     },
     email: {
         type: String, 
-        require: true
+        required: true
     },
     password: {
         type: String, 
-        require: true
-    },
-    photo : {
-        type: String,
-        required: false
+        required: true
     }
-
 });
 
-const Users = mongoose.model("Users", UsersSchema);
+UserSchema.pre('save', function (next) {
+    const User = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(User.password, salt, null, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                User.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
 
-module.exports = Users;
+UserSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
+// const User = mongoose.model("User", UserSchema);
+
+// module.exports = User;
+
+module.exports = mongoose.model('User', UserSchema);
+
