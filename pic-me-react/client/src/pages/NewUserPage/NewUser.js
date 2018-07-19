e11c689e646d1576ffec6d2b92d7411d25250fb3
doreensheen <sheen.doreen@gmail.com>
2018-07-18 21:29:28 -0500

    implement uploadcare to upload user profile pic during new user  creation

--- pic-me-react/client/src/pages/NewUserPage/NewUser.js
+++ pic-me-react/client/src/pages/NewUserPage/NewUser.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import NewUserForm from "../../components/NewUserForm/NewUserForm"
 import axios from 'axios';
 import { Link } from "react-router-dom";
+import Uploader from "../../components/Uploader/Uploader";
+import "./NewUserPage.css"
 
 class NewUser extends Component {
     constructor(props) {
       super(props);
       this.state = {
         userName: '',
-        password: ''
+        password: '',
+        url: 'https://vignette.wikia.nocookie.net/the-darkest-minds/images/4/47/Placeholder.png/revision/latest/scale-to-width-down/480?cb=20160927044640'
       };
     }
     // state = {
@@ -29,24 +32,31 @@ class NewUser extends Component {
       //   [name]: value
       // });
     };
+
+    handleFileUpload = url => {
+      console.log("String");
+      this.setState({url})
+      console.log(url)
+    }
     
     handleFormSubmit = event => {
       event.preventDefault();
       
-      const { userName, password } = this.state;
-      console.log(userName, password);
+      const { userName, password, url } = this.state;
+      console.log(userName, password, url);
 
-      axios.post('/api/auth/register', { userName, password })
+      axios.post('/api/auth/register', { userName, password, url })
         .then((result) => {
           console.log(userName);
           console.log(password);
+          console.log(url)
           console.log(result);
           this.props.history.push("/login");
         })
-      // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-      // alert(`Hello ${this.state.userName}. Your password is "${this.state.password}".`);
-    };
-
+        // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
+        // alert(`Hello ${this.state.userName}. Your password is "${this.state.password}".`);
+      };
+      
     render() {
       return (
         <div>
@@ -63,10 +73,12 @@ class NewUser extends Component {
                   <label htmlFor="loginPassword1Create">Enter New Password</label>
                   <input name="password1" value={this.state.password} onChange={this.handleInputChange("password")} type="text" className="form-control" id="loginPassword1Create" placeholder="" required></input>
               </div>
-              {/* <div className="form-group">
-                  <label for="loginPassword2Create">Re-Enter New Password</label>
-                  <input name="password2" value={this.state.password2} onChange={this.handleInputChange} type="text" className="form-control" id="loginPassword2Create" placeholder=""></input>
-              </div> */}
+              <div><img id="image-preview" src={this.state.url}></img></div>
+              <Uploader id="uploader" onChange={this.handleFileUpload}  name="my_file" data-images-only data-tabs="file camera url" data-crop="1:1"/>
+
+              <br></br>
+              <br></br>
+
               <button onClick={this.handleFormSubmit} className="btn btn-primary">Submit</button>
           </form>
           </div>
