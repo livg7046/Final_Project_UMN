74fe29e9b9a2e0847f0a58c7f6ec8a58d7387a25
Dan <dpedersen84@gmail.com>
2018-07-31 21:44:47 -0500

    server edit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')))
 // Routes
 const photo = require('./routes/photos');
 // const likes = require('./routes/likes');
