391212d021ee59447b71a76a962f706869ccfe16
Dan <dpedersen84@gmail.com>
2018-07-31 22:33:32 -0500

    new build attempt

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 // app.get('*', (req, res) => res.sendFile(path.resolve('client/public', 'index.html')))
 // Routes
