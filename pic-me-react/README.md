c999fdb87429c4b9ea02095e7bcf42f73869f2cf
Dan <dpedersen84@gmail.com>
2018-07-31 21:16:16 -0500

    routing changes to fix heroku deployment and auth errors

--- pic-me-react/README.md
+++ pic-me-react/README.md
@@ -1,1 +1,23 @@
-# Final_Project_UMN
+Overview
+
+PicMe is an app that allows a user to answer a daily question and find a random GIF that matches what they submitted for an answer.  The GIF can then be posted to the Global page where other users can comment and like the photo.  The goal is to get as many likes on your GIF as possible. A new question will be posted at midnight allowing the user to participate again and trying to receive as many likes as possible. 
+Motivation
+This projects exists because it’s a fun way for internet users to connect with others while commenting and liking each others  GIF’s.  It allows users to take sometime away from their daily life and just have a little fun. 
+Installation
+
+Below are a list of installs you must add in the command line in order for the app to work.
+Npm, axios, cherrio, morgan mongoose, passport, react-router-dom, react-router-browser, body-parser, jquery, if-enf, moment, bcrypt. 
+Links to get started
+https://www.codecademy.com/articles/how-to-create-a-react-app
+https://reactjs.org/ 
+
+Contributors
+Dan Pedersen
+Doreen Sheen
+Heidi Larson
+Olivia Graham
+Kevin Widmer
+
+License
+MIT 
+https://opensource.org/licenses/MIT
--- pic-me-react/client/src/App.js
+++ pic-me-react/client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, } from "react-router-dom";
 import './App.css';
-import Title from "./components/Title";
+// import Title from "./components/Title";
 import TitleOnly from "./components/TitleOnly";
 import Question from './pages/QuestionPage';
 import Wrapper from "./components/Wrapper";
@@ -9,32 +9,32 @@ import Login from "./pages/LoginPage/Login";
 import NewUser from "./pages/NewUserPage/NewUser";
 import UserPage from "./pages/UserPage";
 import GlobalPage from './pages/Global/Global';
-import axios from 'axios';
+// import axios from 'axios';
 
 class App extends Component {
-  state = {
-    user: [],
-    profilePic: '',
-    image: "https://ucarecdn.com/eda32654-f96e-4081-86b6-11cf46d8d05f/-/crop/1735x1738/1,0/-/preview/"
+  // state = {
+  //   user: [],
+  //   profilePic: '',
+  //   image: "https://ucarecdn.com/eda32654-f96e-4081-86b6-11cf46d8d05f/-/crop/1735x1738/1,0/-/preview/"
 
-  }
+  // }
 
-  componentDidMount = () => {
-    this.getUsers();
-  }
+  // componentDidMount = () => {
+  //   this.getUsers();
+  // }
 
-  getUsers() {
-    axios.get(`/api/auth/users/${localStorage.getItem('userId')}`)
-    .then(res => {
-      const user = res.data;
-      this.setState({ user: user });
-      console.log(user, "user data");
-      // console.log(this.state.user)
-      const mappingFunction = p => p.profileUrl;
-      // const url = ((this.state.user).map(mappingFunction));
-      this.setState({ profilePic: (this.state.user).map(mappingFunction)})
-      })
-  }
+  // getUsers() {
+  //   axios.get(`/api/auth/users/${localStorage.getItem('userId')}`)
+  //   .then(res => {
+  //     const user = res.data;
+  //     this.setState({ user: user });
+  //     console.log(user, "user data");
+  //     // console.log(this.state.user)
+  //     const mappingFunction = p => p.profileUrl;
+  //     // const url = ((this.state.user).map(mappingFunction));
+  //     this.setState({ profilePic: (this.state.user).map(mappingFunction)})
+  //     })
+  // }
 
 
   render() {
@@ -67,21 +67,21 @@ class App extends Component {
 
           <Route exact path="/question" render={(props) => (
             <div>
-            <Title src={(this.state.profilePic)}/>
+            {/* <Title src={(this.state.profilePic)}/> */}
             <Question history={props.history} /> 
             </div>
           )} />
 
           <Route exact path="/userpage" render={(props) => (
             <div>
-              <Title src={(this.state.profilePic)}/>
+              {/* <Title src={(this.state.profilePic)}/> */}
               <UserPage history={props.history}/>  
               </div>
             ) }/>
 
           <Route exact path="/global" render={(props) => (
             <div>
-              <Title src={(this.state.profilePic)}/>
+              {/* <Title src={(this.state.profilePic)}/> */}
               <GlobalPage history={props.history}/>  
               </div>
             ) }/>
